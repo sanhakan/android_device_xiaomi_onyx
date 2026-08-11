0244c7795c875525d523b704a061da56bdcefdaf
sanhakan <130486232+sanhakan@users.noreply.github.com>
2026-08-11 14:46:45 +0300

    Update and rename crdroid.dependencies to vendorsetup.sh

---
+++ vendorsetup.sh
@@ -0,0 +1,19 @@
+echo 'Starting to clone stuffs needed to build for onyx'
+
+# Kernel Stuff
+echo 'Cloning Kernel Stuff'
+rm -rf kernel/xiaomi/sm8735 && git clone https://github.com/crdroidandroid/android_kernel_xiaomi_sm8735 -b 16.0 kernel/xiaomi/sm8735
+rm -rf kernel/xiaomi/sm8735-modules && git clone https://github.com/crdroidandroid/android_kernel_xiaomi_sm8735-modules.git -b 16.0 kernel/xiaomi/sm8735-modules
+rm -rf kernel/xiaomi/sm8735-devicetrees && git clone https://github.com/crdroidandroid/android_kernel_xiaomi_sm8735-devicetrees.git -b 16.0 kernel/xiaomi/sm8735-devicetrees
+
+# Camera Stuff
+echo 'Cloning Camera Stuff'
+rm -rf device/xiaomi/onyx-miuicamera && git clone https://gitlab.com/crdroidandroid/android_device_xiaomi_miuicamera.git -b 16.0-onyx device/xiaomi/onyx-miuicamera
+rm -rf vendor/xiaomi/onyx-miuicamera && git clone https://gitlab.com/crdroidandroid/proprietary_vendor_xiaomi_miuicamera.git -b 16.0-onyx vendor/xiaomi/onyx-miuicamera
+echo 'Cloning process is completed, now its time for lunch'
+
+# Signig Stuff
+git clone https://github.com/Lafactorial/vendor_lineage-priv_keys -b ascp vendor/custom-priv/keys
+cd vendor/custom-priv/keys
+./keys.sh
+cd ../../../
